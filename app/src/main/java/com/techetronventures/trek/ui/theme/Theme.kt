8d4bedd0e89fea705102a7a6c01ca7b37374f4a5
Mamunur Rashid Johny <mamunur.johny@technonext.com>
2025-01-02 19:04:04 +0600

    Remove unused import

--- app/src/main/java/com/techetronventures/trek/ui/theme/Theme.kt
+++ app/src/main/java/com/techetronventures/trek/ui/theme/Theme.kt
@@ -1,14 +1,10 @@
 package com.techetronventures.trek.ui.theme
 
-import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.dynamicDarkColorScheme
-import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.platform.LocalContext
 
 private val lightScheme = lightColorScheme(
     primary = primaryLight,
@@ -90,7 +86,7 @@ private val darkScheme = darkColorScheme(
 @Composable
 fun TrekTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    content: @Composable() () -> Unit
+    content: @Composable () -> Unit
 ) {
     val colorScheme = when {
         darkTheme -> darkScheme
